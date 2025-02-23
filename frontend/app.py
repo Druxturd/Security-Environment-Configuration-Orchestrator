245a52e930506e646fd520c783e2b05c6d99eaf4
Druxturd <88882970+Druxturd@users.noreply.github.com>
2025-02-23 21:35:44 +0700

    test #1
    python 3.13.2
    
    belajar pyqt5 + fastAPI

---
+++ frontend/app.py
@@ -0,0 +1,31 @@
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("MyApp")
+        
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
+
+        self.setFixedSize(QSize(400, 300))
+
+        self.setCentralWidget(button)
+    
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
+    
+if __name__ == "__main__":
+    app = QApplication([])
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
