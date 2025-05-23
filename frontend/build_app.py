7639db92dd6ef5cb7cd2519a8909e647e9e02c6f
Druxturd <88882970+Druxturd@users.noreply.github.com>
2025-05-23 22:36:47 +0700

    Hot Fix

--- frontend/build_app.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import Executable, setup
-
-if __name__ == "__main__":
-    setup(
-        name="SECOR",
-        version="1.0",
-        description="Security Environment Configuration Orchestrator",
-        executables=[Executable(script="main.py", target_name="SECOR_v1.0")],
-    )
